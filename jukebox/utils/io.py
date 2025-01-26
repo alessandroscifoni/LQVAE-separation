47a34ca2289bfa98b8171672a4437e9d2cd3ad74
scifu <scifoni.1948810@studenti.uniroma1.it>
2025-01-26 22:20:50 +0100

    bug dataprocessor

--- jukebox/utils/io.py
+++ jukebox/utils/io.py
@@ -51,7 +51,7 @@ def load_audio(file, sr, offset, duration, resample=True, approx=False, time_bas
         
         
         print("Frame ", frame)
-        frame = frame.to_ndarray(dict(format='fltp')) # Convert to floats and not int16
+        frame = frame.to_ndarray() # Convert to floats and not int16
         read = frame.shape[-1]
         if total_read + read > duration:
             read = duration - total_read
