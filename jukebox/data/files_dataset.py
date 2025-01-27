8840ae76f8956c1b0b97cca0b71e94c685619223
scifu <scifoni.1948810@studenti.uniroma1.it>
2025-01-27 14:40:43 +0100

    bug dataprocessor

--- jukebox/data/files_dataset.py
+++ jukebox/data/files_dataset.py
@@ -55,7 +55,7 @@ class FilesAudioDataset(Dataset):
         # For a given dataset item and shift, return song index and offset within song
         half_interval = self.sample_length//2
         shift = np.random.randint(-half_interval, half_interval) if self.aug_shift else 0
-        assert isinstance(self.shift, (int, float)), f"shift must be int or float, not {type(self.shift)}"
+        assert isinstance(shift, (int, float)), f"shift must be int or float, not {type(self.shift)}"
         print(f"Shift type {type(shift)}")
         print(f"item type {type(item)}")
         offset = item * self.sample_length + shift # Note we centred shifts, so adding now
