51eaf96b9b4178c978b01170b820a8d037dd06f8
scifu <scifoni.1948810@studenti.uniroma1.it>
2025-02-04 15:18:22 +0100

    adding print to debug

--- jukebox/make_models.py
+++ jukebox/make_models.py
@@ -85,7 +85,7 @@ def make_vqvae(hps, device='cuda'):
         top_raw_to_tokens = np.prod(downsamples)
         hps.sample_length = (hps.sample_length_in_seconds * hps.sr // top_raw_to_tokens) * top_raw_to_tokens
         print(f"Setting sample length to {hps.sample_length} (i.e. {hps.sample_length/hps.sr} seconds) to be multiple of {top_raw_to_tokens}")
-
+    print(f"hps.emb_width: {hps.emb_width}")
     vqvae = VQVAE(input_shape=(hps.sample_length,1), levels=hps.levels, downs_t=hps.downs_t, strides_t=hps.strides_t,
                   emb_width=hps.emb_width, l_bins=hps.l_bins,
                   mu=hps.l_mu, commit=hps.commit,
