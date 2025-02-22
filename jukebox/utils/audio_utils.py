316ac8e81bb153df5139d2f470d62a632dccc26b
scifu <scifoni.1948810@studenti.uniroma1.it>
2025-02-22 20:34:38 +0100

    add print to debug stft

--- jukebox/utils/audio_utils.py
+++ jukebox/utils/audio_utils.py
@@ -30,7 +30,9 @@ def calculate_bandwidth(dataset, hps, duration=600):
             x, y = x
         samples = x.astype(np.float64)
         print(f"sample shape: {samples.shape}")
-        stft = librosa.core.stft(np.mean(samples, axis=1), n_fft=hps.n_fft, hop_length=hps.hop_length, win_length=hps.window_size)
+        if samples.shape[0] == 1:
+            stft = librosa.core.stft(samples.squeeze(), n_fft=hps.n_fft, hop_length=hps.hop_length, win_length=hps.window_size)
+        else: stft = librosa.core.stft(np.mean(samples, axis=1), n_fft=hps.n_fft, hop_length=hps.hop_length, win_length=hps.window_size)
         spec = np.absolute(stft)
         spec_norm_total += np.linalg.norm(spec)
         spec_nelem += 1
