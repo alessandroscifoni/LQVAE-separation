8d521dd26d521847f99f94221a5696bbae60e952
scifu <scifoni.1948810@studenti.uniroma1.it>
2025-02-23 18:40:56 +0100

    deleting other print

--- jukebox/hparams.py
+++ jukebox/hparams.py
@@ -13,10 +13,8 @@ def setup_hparams(hparam_set_names, kwargs):
     if not isinstance(hparam_set_names, tuple):
         hparam_set_names = hparam_set_names.split(",")
     hparam_sets = [HPARAMS_REGISTRY[x.strip()] for x in hparam_set_names if x] + [kwargs]
-    print(f"Using hparam sets: {hparam_sets}")
     for k, v in DEFAULTS.items():
         H.update(v)
-    print(f"Using defaults: {DEFAULTS}")
     for hps in hparam_sets:
         for k in hps:
             if k not in H:
