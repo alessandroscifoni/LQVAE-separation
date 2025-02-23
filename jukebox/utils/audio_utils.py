fb835d19686fc590b48f44983a2ccf4f9d622284
scifu <scifoni.1948810@studenti.uniroma1.it>
2025-02-23 16:41:28 +0100

    deleting some print

--- jukebox/utils/audio_utils.py
+++ jukebox/utils/audio_utils.py
@@ -29,14 +29,10 @@ def calculate_bandwidth(dataset, hps, duration=600):
         if isinstance(x, (tuple, list)):
             x, y = x
         samples = x.astype(np.float64)
-        # print(f"sample shape: {samples.shape}")
         if samples.shape[0] == 1:
             stft = librosa.core.stft(np.mean(samples, axis=0), n_fft=hps.n_fft, hop_length=hps.hop_length, win_length=hps.window_size)
-            print(f"stft shape: {stft.shape}")
-            print(f"stft: {stft}")
         else: stft = librosa.core.stft(np.mean(samples, axis=1), n_fft=hps.n_fft, hop_length=hps.hop_length, win_length=hps.window_size)
         spec = np.absolute(stft)
-        print(f" spec {spec}")
         spec_norm_total += np.linalg.norm(spec)
         spec_nelem += 1
         n_seen += int(np.prod(samples.shape))
